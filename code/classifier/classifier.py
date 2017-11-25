6df6de84d11ebe9919e2a5fb80679b355ba3ca13
Aditya Nanjangud <aditya.ngud@gmail.com>
2017-11-24 18:05:31 -0600

    Ensemble classifier

--- code/classifier/classifier.py
+++ code/classifier/classifier.py
@@ -13,8 +13,15 @@ import logging
 import time
 import scipy.sparse as sp
 
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO)
 start_time = time.time()
+logging.info(" ----------- Start Detector ------------")
+
 train_data = pd.read_csv('../../data/combined_features.csv')
+
+
+f = open("results.log", "a")
+f.write("\n\n ---------------------------- BEGIN EXECUTION ---------------------------- ")
 
 train_notes_features = []
 train_notes_features.append(np.array(train_data.Topic1))
@@ -100,17 +107,41 @@ for i in range(len(test_data_features_list)):
 #Classifiers
 svm = svm.SVC(C=0.3,kernel='linear',probability=True, verbose=True)
 lr = LogisticRegression(random_state=1, verbose=True)
+rfc = RandomForestClassifier(random_state=1)
 
 # Ensemble Classifier
 eclf = VotingClassifier(estimators=[
-    ('svm', svm), ('lr', lr)
-], voting='soft', weights=[0.6, 0.4])
+    ('svm', svm), ('lr', lr), ('rfc', rfc)
+], voting='soft', weights=[0.3, 0.3, 0.4])
 
 # Training
-#eclf.fit(training_data_features_list, training_output)
-svm.fit(training_data_features_list, training_output)
+eclf.fit(training_data_features_list, training_output)
+#svm.fit(training_data_features_list, training_output)
 # Accuracy score
 accuracy_score = svm.score(test_data_features_list, test_output)
-score_str = "Score = %s" % accuracy_score
+score_str = "Accuracy Score = %s" % accuracy_score
+f.write("\n" + score_str)
 
 print(score_str)
+
+predictions = eclf.predict_proba(test_data_features_list)
+predict_ans = eclf.predict(test_data_features_list)
+expired_predictions = predictions[:, 1]
+false_positive_rate, true_positive_rate, thresholds = roc_curve(test_output, expired_predictions)
+auc_roc = auc(false_positive_rate, true_positive_rate)
+auc_str = "AUC_ROC = %s" % auc_roc
+f.write("\n" + auc_str)
+print("Test data auc(roc curve) : ", auc_roc)
+
+precision, recall, thresholds = precision_recall_curve(test_output, expired_predictions)
+print("Test data auc(PR curve) : ", auc(recall, precision))
+print("(PRF)macro : ", precision_recall_fscore_support(test_output, predict_ans, average='macro'))
+print("(PRF)micro : ", precision_recall_fscore_support(test_output, predict_ans, average='micro'))
+print("(PRF) : ", precision_recall_fscore_support(test_output, predict_ans, labels=[0, 1]))
+f.write("\n\n ---------------------------- END EXECUTION ---------------------------- \n\n")
+f.close()
+logging.info(" ----------- End Detector ------------")
+total_time = time.time() - start_time
+m, s = divmod(total_time, 60)
+h, m = divmod(m, 60)
+print("Program run time: %d:%02d:%02d" % (h, m, s))
